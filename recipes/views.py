6561ba495fe3563880468ec192399473e012cb57
JoaoPedroCavalcanti <joao.cavalcanti.703@gmail.com>
2024-07-17 11:46:02 -0300

    contact and about deleted

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def home(req):
     return render(req, 'recipes/home.html')
 
-def about(req):
-    return HttpResponse("Recipe about")
-    
-def contact(req):
-    return HttpResponse("Recipe contact")
